59de8397ec4a567de790cbdb5cf2e3c237f2b9e3
umeshbsa <umesh.bsa@gmail.com>
2019-03-15 10:55:16 +0530

    Code refresh

--- app/src/main/java/com/app/es/LauncherActivity11.java
+++ app/src/main/java/com/app/es/LauncherActivity11.java
@@ -1,29 +1,20 @@
 package com.app.es;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
-import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Toast;
 
 import com.hankang.phone.treadmill.R;
 
-public class LauncherActivity11 extends Activity implements OnClickListener {
+public class LauncherActivity11 extends Activity {
     private static final String TAG = "LauncherActivity11";
-
-
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,54 +23,14 @@ public class LauncherActivity11 extends Activity implements OnClickListener {
         setContentView(R.layout.launcher_activity);
 
         initTreadmillBlueTooth();
-    } ServiceConnection mServiceConnection = new ServiceConnection() {
-        public void onServiceConnected(ComponentName componentName, IBinder service) {
-            GVariable.bluetoothLeService = ((BluetoothTreadmillService.LocalBinder) service).getService();
-            if (GVariable.bluetoothLeService.initialize()) {
-                GVariable.bluetoothLeService.connect(GVariable.treadmillDevice);
-
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-
-                        LauncherActivity11.this.startActivity(new Intent(LauncherActivity11.this, PlayActivity.class));
-
-
-                    }
-                }, 2000);
-                return;
-            }
-        }
-
-        public void onServiceDisconnected(ComponentName componentName) {
-        }
-    };
-
-
-    protected void onDestroy() {
-        GVariable.activityList.remove(this);
-        super.onDestroy();
     }
 
 
     private void initTreadmillBlueTooth() {
         if (!getPackageManager().hasSystemFeature("android.hardware.bluetooth_le")) {
             Toast.makeText(this, R.string.supportble, Toast.LENGTH_LONG).show();
-           // finish();
         }
-
         scanBleDevice();
-      /*
-        BluetoothAdapter mBluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
-            String device = PreferenceUtil.getString(this, PreferenceUtil.KEY_DEVICE_TREADMILL, "");
-            LogUtil.m296w(TAG, "initTreadmillBlueTooth", "device=" + device);
-            if (TextUtils.isEmpty(device)) {
-                scanBleDevice();
-                return;
-            }
-            GVariable.treadmillDevice = device;
-            initBleService();
-        */
     }
 
     private void scanBleDevice() {
@@ -108,10 +59,32 @@ public class LauncherActivity11 extends Activity implements OnClickListener {
         Log.w(TAG, "Try to bindService=" + bindService(new Intent(this, BluetoothTreadmillService.class), this.mServiceConnection, Context.BIND_AUTO_CREATE));
     }
 
-    public void onClick(View v) {
-        switch (v.getId()) {
+    ServiceConnection mServiceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            GVariable.bluetoothLeService = ((BluetoothTreadmillService.LocalBinder) service).getService();
+            if (GVariable.bluetoothLeService.initialize()) {
+                GVariable.bluetoothLeService.connect(GVariable.treadmillDevice);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        LauncherActivity11.this.startActivity(new Intent(LauncherActivity11.this, PlayActivity.class));
 
 
+                    }
+                }, 2000);
+                return;
+            }
         }
+
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
+
+
+    protected void onDestroy() {
+        GVariable.activityList.remove(this);
+        super.onDestroy();
     }
 }
