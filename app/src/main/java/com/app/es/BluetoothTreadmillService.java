bc08245ef7cb98553bd09821e64e4c965338fc17
umeshbsa <umesh.bsa@gmail.com>
2019-03-15 11:14:35 +0530

    Code refresh

--- app/src/main/java/com/app/es/BluetoothTreadmillService.java
+++ app/src/main/java/com/app/es/BluetoothTreadmillService.java
@@ -9,12 +9,12 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -34,23 +34,16 @@ public class BluetoothTreadmillService extends Service {
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothGatt mBluetoothGatt;
     private BluetoothManager mBluetoothManager;
-    private final BluetoothGattCallback mGattCallback = new C02481();
     public BluetoothGattCharacteristic mNotifyCharacteristic;
     public BluetoothGattCharacteristic mWriteCharacteristic;
 
-    /* renamed from: com.hankang.phone.treadmill.service.BluetoothTreadmillService$1 */
-    class C02481 extends BluetoothGattCallback {
-        C02481() {
-        }
+    BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
 
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            LogUtil.m294i(BluetoothTreadmillService.TAG, "mGattCallback", "status=" + status + "//newState=" + newState);
             if (newState == 2) {
                 String intentAction = BluetoothTreadmillService.ACTION_GATT_CONNECTED;
-                LogUtil.m294i(BluetoothTreadmillService.TAG, "mGattCallback", "discoverServices=" + BluetoothTreadmillService.this.mBluetoothGatt.discoverServices());
                 BluetoothTreadmillService.this.broadcastUpdate(intentAction);
             } else if (newState == 0) {
-                LogUtil.m294i(BluetoothTreadmillService.TAG, "mGattCallback", "ACTION_GATT_DISCONNECTED");
                 GVariable.isConnected = false;
                 GVariable.isStart = false;
                 BluetoothTreadmillService.this.broadcastUpdate(BluetoothTreadmillService.ACTION_GATT_DISCONNECTED);
@@ -58,16 +51,13 @@ public class BluetoothTreadmillService extends Service {
         }
 
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-            LogUtil.m296w(BluetoothTreadmillService.TAG, "onServicesDiscovered", "status=" + status);
             if (status == 0) {
                 BluetoothTreadmillService.this.findService();
             } else if (BluetoothTreadmillService.this.mBluetoothGatt.getDevice().getUuids() == null) {
-                LogUtil.m296w(BluetoothTreadmillService.TAG, "onServicesDiscovered", "mBluetoothGatt.getDevice().getUuids() == null");
             }
         }
 
         public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-            LogUtil.m296w(BluetoothTreadmillService.TAG, "onCharacteristicRead", "characteristic=" + Arrays.toString(characteristic.getValue()));
             BluetoothTreadmillService.this.broadcastData(BluetoothTreadmillService.ACTION_DATA_AVAILABLE, characteristic.getValue());
         }
 
@@ -81,22 +71,18 @@ public class BluetoothTreadmillService extends Service {
         }
 
         public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor bd, int status) {
-            Log.e(BluetoothTreadmillService.TAG, "onDescriptorRead");
         }
 
         public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor bd, int status) {
-            Log.e(BluetoothTreadmillService.TAG, "onDescriptorWrite");
         }
 
         public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
             BluetoothTreadmillService.this.ble_rssi = rssi;
-            LogUtil.m294i(BluetoothTreadmillService.TAG, "onReadRemoteRssi", "ble_rssi=" + BluetoothTreadmillService.this.ble_rssi);
         }
 
         public void onReliableWriteCompleted(BluetoothGatt gatt, int a) {
-            LogUtil.m293e(BluetoothTreadmillService.TAG, "onReliableWriteCompleted", "a=" + a);
         }
-    }
+    };
 
     public class LocalBinder extends Binder {
         public BluetoothTreadmillService getService() {
@@ -107,12 +93,10 @@ public class BluetoothTreadmillService extends Service {
     public boolean WriteValue(byte[] value) {
         BluetoothGattCharacteristic characteristic = this.mWriteCharacteristic;
         if (this.mWriteCharacteristic == null) {
-            LogUtil.m296w(TAG, "WriteValue", "mWriteCharacteristic == null");
             GVariable.isConnected = false;
             GVariable.isStart = false;
             return false;
         } else if (this.mBluetoothGatt == null) {
-            LogUtil.m296w(TAG, "WriteValue", "mBluetoothGatt == null");
             GVariable.isConnected = false;
             GVariable.isStart = false;
             return false;
@@ -124,14 +108,12 @@ public class BluetoothTreadmillService extends Service {
             }
             characteristic.setValue(value);
             boolean write = this.mBluetoothGatt.writeCharacteristic(characteristic);
-            LogUtil.m294i(TAG, "WriteValue", "write=" + write);
             return write;
         }
     }
 
     public void findService() {
         if (this.mBluetoothGatt == null) {
-            LogUtil.m292d(TAG, "findService", "mBluetoothGatt==null");
             return;
         }
         boolean findTX = false;
@@ -142,7 +124,6 @@ public class BluetoothTreadmillService extends Service {
                 BluetoothGattCharacteristic gattCharacteristic = (BluetoothGattCharacteristic) gattCharacteristics.get(j);
                 int charaProperty = gattCharacteristic.getProperties();
                 if (UUID_ISSC_CHAR_RX.equals(gattCharacteristic.getUuid())) {
-                    LogUtil.m294i(TAG, "findService", "UUID_ISSC_CHAR_RX=" + gattCharacteristic.getUuid().toString());
                     if ((charaProperty & 16) > 0) {
                         this.mNotifyCharacteristic = gattCharacteristic;
                         this.mBluetoothGatt.setCharacteristicNotification(gattCharacteristic, true);
@@ -152,13 +133,11 @@ public class BluetoothTreadmillService extends Service {
                     this.mBluetoothGatt.writeDescriptor(descriptor);
                 } else if (findTX || !UUID_ISSC_CHAR_TX.equals(gattCharacteristic.getUuid())) {
                     if (!findTX && UUID_ISSC_CHAR_5TX.equals(gattCharacteristic.getUuid()) && ((charaProperty & 8) > 0 || (charaProperty & 4) > 0)) {
-                        LogUtil.m294i(TAG, "findService", " UUID_ISSC_CHAR_5TX=" + gattCharacteristic.getUuid().toString());
                         this.mWriteCharacteristic = gattCharacteristic;
                         broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
                         findTX = true;
                     }
                 } else if ((charaProperty & 8) > 0 || (charaProperty & 4) > 0) {
-                    LogUtil.m294i(TAG, "findService", " UUID_ISSC_CHAR_TX=" + gattCharacteristic.getUuid().toString());
                     this.mWriteCharacteristic = gattCharacteristic;
                     broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
                     findTX = true;
@@ -169,16 +148,6 @@ public class BluetoothTreadmillService extends Service {
 
     private void broadcastUpdate(String action) {
         sendBroadcast(new Intent(action));
-    }
-
-    public int getRSSI() {
-        return this.ble_rssi;
-    }
-
-    public void update_rssi() {
-        if (this.mBluetoothGatt != null) {
-            this.mBluetoothGatt.readRemoteRssi();
-        }
     }
 
     private void broadcastData(String action, byte[] data) {
@@ -200,77 +169,46 @@ public class BluetoothTreadmillService extends Service {
 
     public boolean initialize() {
         if (this.mBluetoothManager == null) {
-            this.mBluetoothManager = (BluetoothManager) getSystemService("bluetooth");
+            this.mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
             if (this.mBluetoothManager == null) {
-                Log.e(TAG, "Unable to initialize BluetoothManager.");
-                return false;
+                 return false;
             }
         }
         this.mBluetoothAdapter = this.mBluetoothManager.getAdapter();
         if (this.mBluetoothAdapter != null) {
             return true;
         }
-        Log.e(TAG, "Unable to obtain a BluetoothAdapter.");
+
         return false;
     }
 
     public boolean connect(String address) {
-        LogUtil.m296w(TAG, "connect", "address=" + address);
-        if (this.mBluetoothAdapter == null || address == null) {
-            Log.w(TAG, "BluetoothAdapter not initialized or unspecified address.");
-            return false;
+         if (this.mBluetoothAdapter == null || address == null) {
+             return false;
         }
         BluetoothDevice device = this.mBluetoothAdapter.getRemoteDevice(address);
         if (device == null) {
-            Log.w(TAG, "Device not found.  Unable to connect.");
-            return false;
+             return false;
         }
         if (this.mBluetoothGatt != null) {
             this.mBluetoothGatt.disconnect();
             this.mBluetoothGatt = null;
         }
         this.mBluetoothGatt = device.connectGatt(this, true, this.mGattCallback);
-        LogUtil.m296w(TAG, "connect", " mBluetoothGatt=" + this.mBluetoothGatt);
-        return true;
+         return true;
     }
 
     public void disconnect() {
         if (this.mBluetoothAdapter == null || this.mBluetoothGatt == null) {
-            Log.w(TAG, "BluetoothAdapter not initialized");
-            return;
+             return;
         }
         this.mBluetoothGatt.disconnect();
-        LogUtil.m296w(TAG, "disconnect", "disconnect()");
-    }
+     }
 
     public void close() {
-        LogUtil.m296w(TAG, "close()", "close()");
-        if (this.mBluetoothGatt != null) {
+         if (this.mBluetoothGatt != null) {
             this.mBluetoothGatt.close();
             this.mBluetoothGatt = null;
         }
-    }
-
-    public void readCharacteristic(BluetoothGattCharacteristic characteristic) {
-        if (this.mBluetoothAdapter == null || this.mBluetoothGatt == null) {
-            Log.w(TAG, "BluetoothAdapter not initialized");
-        } else {
-            this.mBluetoothGatt.readCharacteristic(characteristic);
-        }
-    }
-
-    public void setCharacteristicNotification(BluetoothGattCharacteristic characteristic, boolean enabled) {
-        if (this.mBluetoothAdapter == null || this.mBluetoothGatt == null) {
-            Log.w(TAG, "BluetoothAdapter not initialized");
-        } else {
-            this.mBluetoothGatt.setCharacteristicNotification(characteristic, enabled);
-        }
-    }
-
-    public List<BluetoothGattService> getSupportedGattServices() {
-        if (this.mBluetoothGatt == null) {
-            return null;
-        }
-        return this.mBluetoothGatt.getServices();
     }
 }
